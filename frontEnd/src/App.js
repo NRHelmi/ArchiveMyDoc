52a608579304f59bafffdd44805ef854d40a39b4
nrhelmi <helmiinour@gmail.com>
2017-10-17 22:27:09 +0000

    Linking backend to frontend through rest(axios)

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends Component{
@@ -7,9 +8,10 @@ class App extends Component{
     <div>
         <BrowserRouter>
             <div>
-                    <Route path="/"       component={Home}/>
+                    <Route path="/"        component={Home}/>
                     <Route path="/hello1"  component={HelloWorld}/>
                     <Route path="/hello2"  component={HelloWorld2}/>
+                    <Route path="/restTest"component={RestCall}/>
             </div>
         </BrowserRouter>
     </div>
@@ -25,6 +27,7 @@ class Home extends Component {
           <li><a href="/">Home</a></li>
           <li><a href="/hello1">First</a></li>
           <li><a href="/hello2">Second</a></li>
+          <li><a href="/restTest">RestTest</a></li>
         </ul>
         <h1> Welcome to ArchiveMyDoc </h1>
       </div>
@@ -52,5 +55,26 @@ class HelloWorld2 extends Component{
   }
 }
 
+class RestCall extends Component{
+  constructor(props){
+      super(props);
+
+      this.state = {
+          message: ''
+      }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:8080/')
+    .then(response => {
+      console.log(response)
+      this.setState({ message: response.data })
+    })
+    .catch(err => console.log(err))
+  }
+  render(){
+    return(
+      <h1>{this.state.message}</h1>
+    )}
+}
 
 export default App;
