a01e2e20ba9e4b6cd67a6d9566ee104024fef745
nrhelmi <helmiinour@gmail.com>
2017-10-17 20:59:57 +0000

    Initializing backend

---
+++ backend/src/main/java/com/archiveMyDoc/Controller/Controller.java
@@ -0,0 +1,12 @@
+package com.archiveMyDoc.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller{
+	@RequestMapping("/")
+	public String HelloWorld() {
+		return "Hello World !!";
+	}
+}
